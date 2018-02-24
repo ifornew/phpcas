2370de0026b7c5714277f0974208a43ac069c6b8
Iwannamaybe <iwannamaybe@vip.qq.com>
2018-02-24 14:39:05 +0800

    init for Packagelist

---
+++ src/PhpCasFacade.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TouchWorld
+ * Date: 2018/2/24
+ * Time: 14:38
+ */
+
+namespace Iwannamaybe\PhpCas;
+
+
+class PhpCasFacade
+{
+
+}
---
+++ src/PhpCasServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TouchWorld
+ * Date: 2018/2/24
+ * Time: 14:37
+ */
+
+namespace Iwannamaybe\PhpCas;
+
+
+class PhpCasServiceProvider
+{
+
+}
