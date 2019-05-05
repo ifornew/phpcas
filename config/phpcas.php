e900ce6e4d8f4e2b39c5fffbf46066e1fa5cc853
iwannamaybe <iwannamaybe@vip.qq.com>
2019-05-05 21:14:44 +0800

    fix config

--- config/phpcas.php
+++ config/phpcas.php
@@ -1,48 +1,12 @@
 <?php
 
 return [
-		/*
-		|--------------------------------------------------------------------------
-		| PHPCas Fake
-		|--------------------------------------------------------------------------
-		*/
-		'cas_fake' => env('CAS_FAKE', false),
-
-		'cas_fake_user_id' => env('CAS_FAKE_USER_ID', 1),
-
-		/*
-		|--------------------------------------------------------------------------
-		| PHPCas UdfProxy
-		|--------------------------------------------------------------------------
-		*/
-		'cas_udf_proxy' => env('CAS_UDF_PROXY', false),
-		'cas_udf_proxy_ip' => '127.0.0.1',
-
-		'cas_version' => env('CAS_VERSION', '2.0'),
-
-		/*
-		|--------------------------------------------------------------------------
-		| PHPCas Hostname
-		|--------------------------------------------------------------------------
-		|
-		| Example: 'cas.myuniv.edu'.
-		|
-		*/
-		'cas_hostname' => env('CAS_HOSTNAME'),
-
-
-		/*
-		|--------------------------------------------------------------------------
-		| Cas Port
-		|--------------------------------------------------------------------------
-		|
-		| Usually 443 is default
-		|
-		*/
-		'cas_port' => env('CAS_PORT', 443),
-
-		'cas_channel' => env('CAS_CHANNEL', 1),
-
+	/*
+	|--------------------------------------------------------------------------
+	| PHPCas Guard
+	|--------------------------------------------------------------------------
+	*/
+	'web' => [
 		/*
 		|--------------------------------------------------------------------------
 		| CAS URI
@@ -51,42 +15,13 @@ return [
 		| Sometimes is /cas
 		|
 		*/
-		'cas_login_uri' => '/login',
-		'cas_logout_uri' => '/logout',
-		'cas_register_uri' => '/crm/platform/register.html',
-		'cas_find_password_uri' => '',
-
-		/*
-		|--------------------------------------------------------------------------
-		| CAS Except url or route
-		|--------------------------------------------------------------------------
-		|
-		| .
-		|
-		*/
-		'cas_except' => [
-				'url' => [
-						'_debugbar'
-				],
-				'route' => [
-
-				]
-		],
-
-		'cas_guard_key' => 'guard',
-
-		/*
-		|--------------------------------------------------------------------------
-		| CAS GUARD
-		|--------------------------------------------------------------------------
-		|
-		| Sometimes is /cas
-		|
-		*/
-		'cas_guard' => [
-				'front' => '/cas',
-				'backend' => '/cas-admin'
-		],
+		'server'            => 'http://172.16.30.60:8080/cas',
+		'login_uri'         => '',
+		'logout_uri'        => '/logout',
+		'register_uri'      => '/crm/platform/register.html',
+		'find_password_uri' => '',
+		'version'           => '2.0',
+		'channel'           => env('APP_NAME', null),
 
 		/*
 		|--------------------------------------------------------------------------
@@ -96,9 +31,8 @@ return [
 		| Path to the CAS certificate file
 		|
 		*/
-		'cas_cert' => env('CAS_CERT', ''),
-
-		'cas_validate' => env('CAS_VALIDATE', false),
+		'cert'              => null,
+		'cert_validate'     => false,
 
 		/*
 		|--------------------------------------------------------------------------
@@ -109,10 +43,33 @@ return [
 		| certificate, empty for no SSL validation.
 		|
 		*/
+		'cert_cn_validate'  => false,
+		'session_key'       => 'phpcas',
+		'ticket_key'        => 'ticket',
 
-		'cas_cert_cn_validate' => env('CAS_CERT_CN_VALIDATE', false),
+		/*
+		|--------------------------------------------------------------------------
+		| CAS Except url or route
+		|--------------------------------------------------------------------------
+		|
+		| .
+		|
+		*/
+		'except'            => [
+			'url'   => [
+				'_debugbar'
+			],
+			'route' => [
 
-		'cas_session_key' => env('CAS_SESSION_KEY', 'phpCas'),
+			]
+		],
 
-		'cas_ticket_key' => env('CAS_TICKET_KEY', 'ticket')
+		/*
+		|--------------------------------------------------------------------------
+		| PHPCas Fake
+		|--------------------------------------------------------------------------
+		*/
+		'fake'              => false,
+		'fake_user_id'      => 18602301175
+	]
 ];
